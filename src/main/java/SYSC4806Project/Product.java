90b11af1d56e254ebf7567e73f6ba5d6ed1187d8
Arthur Atangana <arthur.atangana@protonmail.com>
2024-11-10 13:37:19 -0500

    Add annotations to classes
    - define relationships
    - define table name for user class as "app_user" since user is a reserved keyword.
    - remove extend arraylist from ItemQuantityList as incompatible with springboot
    - separate ItemQuantityList and ItemQuantityPair for better spring boot readibility.

--- src/main/java/SYSC4806Project/Product.java
+++ src/main/java/SYSC4806Project/Product.java
@@ -1,40 +1,60 @@
 package SYSC4806Project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.ArrayList;
 import java.util.List;
+
+// TODO: protect against negative prices
 
 /**
  * Products have a name and price that they can be sold for. Tags help buyers find types of products.
  */
+@Entity
 public class Product {
-    private final String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
     private double price;
-    private final List<Tag> tags = new ArrayList<>();
+    private List<Tag> tags = new ArrayList<>();
 
     public Product(String name, double price) {
         this.name = name;
-        if (price < 0) {
-            this.price = 0; // free
-            System.out.println("Negative price is not allowed. Price is set to 0.");
-        } else {
-            this.price = price;
-        }
+        this.price = price;
+    }
+
+    public Product() {}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        if (price < 0) {
-            System.out.println("Negative price is not allowed. Price has not been changed.");
-        } else {
-            this.price = price;
-        }
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
     }
 
     public List<Tag> getTags() {
@@ -45,4 +65,5 @@ public class Product {
     public String toString() {
         return "Product [name=" + name + ", price=" + price + ", tags=" + tags + "]";
     }
+
 }
