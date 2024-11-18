53e2ed4572f6d74e91ea07e8c00f384ecfed1968
npono21 <nickpon123@gmail.com>
2024-11-18 18:47:40 -0500

    trying to make sure js script is read #46

--- src/main/resources/static/js/login_with_email.js
+++ src/main/resources/static/js/login_with_email.js
@@ -1,4 +1,4 @@
-function toggleEmailForm() {
+window.onload = function () {
   const emailLoginFormDiv = document.getElementById("email-login-div");
   const goBackIconDiv = document.getElementById("go-back-div");
   const emailLoginButton = document.getElementsByClassName("login-button");
@@ -6,21 +6,50 @@ function toggleEmailForm() {
 
   const isHidden = getComputedStyle(emailLoginFormDiv).display === "none";
 
-  if (isHidden) {
-    // Show the form and hide other elements
-    emailLoginFormDiv.style.display = "flex";
-    if (emailLoginButton.length > 0) {
-      emailLoginButton[0].style.display = "none";
-      socialLoginDiv[0].style.display = "none";
-      goBackIconDiv.style.display = "none";
+  emailLoginButton.onclick = function () {
+    if (isHidden) {
+      // Show the form and hide other elements
+      emailLoginFormDiv.style.display = "flex";
+      if (emailLoginButton.length > 0) {
+        emailLoginButton[0].style.display = "none";
+        socialLoginDiv[0].style.display = "none";
+        goBackIconDiv.style.display = "none";
+      }
+    } else {
+      // Hide the form and show other elements
+      emailLoginFormDiv.style.display = "none";
+      if (emailLoginButton.length > 0) {
+        emailLoginButton[0].style.display = "block";
+        socialLoginDiv[0].style.display = "flex";
+        goBackIconDiv.style.display = "block";
+      }
     }
-  } else {
-    // Hide the form and show other elements
-    emailLoginFormDiv.style.display = "none";
-    if (emailLoginButton.length > 0) {
-      emailLoginButton[0].style.display = "block";
-      socialLoginDiv[0].style.display = "flex";
-      goBackIconDiv.style.display = "block";
-    }
-  }
-}
+  };
+};
+
+// function toggleEmailForm() {
+//   const emailLoginFormDiv = document.getElementById("email-login-div");
+//   const goBackIconDiv = document.getElementById("go-back-div");
+//   const emailLoginButton = document.getElementsByClassName("login-button");
+//   const socialLoginDiv = document.getElementsByClassName("social-login-div");
+
+//   const isHidden = getComputedStyle(emailLoginFormDiv).display === "none";
+
+//   if (isHidden) {
+//     // Show the form and hide other elements
+//     emailLoginFormDiv.style.display = "flex";
+//     if (emailLoginButton.length > 0) {
+//       emailLoginButton[0].style.display = "none";
+//       socialLoginDiv[0].style.display = "none";
+//       goBackIconDiv.style.display = "none";
+//     }
+//   } else {
+//     // Hide the form and show other elements
+//     emailLoginFormDiv.style.display = "none";
+//     if (emailLoginButton.length > 0) {
+//       emailLoginButton[0].style.display = "block";
+//       socialLoginDiv[0].style.display = "flex";
+//       goBackIconDiv.style.display = "block";
+//     }
+//   }
+// }
