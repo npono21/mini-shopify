a6861fdf3a3d2cebe8f0c77415b6a20bc2fd57ed
npono21 <nickpon123@gmail.com>
2024-11-18 19:14:34 -0500

    making go back buttons work

--- src/main/resources/static/js/go_back_arrow.js
+++ src/main/resources/static/js/go_back_arrow.js
@@ -2,6 +2,6 @@ window.onload = function () {
   const goBackDiv = document.getElementById("go-back-div");
   goBackDiv.style.cursor = "pointer";
   goBackDiv.onclick = function () {
-    window.location.href = "/index";
+    window.history.back();
   };
 };
