5ab823fd32c9b7fbc05399d528a894dd8e288567
npono21 <nickpon123@gmail.com>
2024-11-18 23:35:06 -0500

    Deleting js file for go back arrow code

--- src/main/resources/static/js/go_back_arrow.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  const goBackDiv = document.getElementById("go-back-div");
-  goBackDiv.style.cursor = "pointer";
-  goBackDiv.onclick = function () {
-    window.history.back();
-  };
-});
