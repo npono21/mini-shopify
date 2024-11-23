1693ffdc82acf931a2d7fb18f4c34838e170cd80
npono21 <nickpon123@gmail.com>
2024-11-23 13:03:24 -0500

    Trying to get db to work #50

--- src/main/resources/templates/merchant_register.php
+++ src/main/resources/templates/merchant_register.php
@@ -1,12 +1,18 @@
 <?php
-    $serverName = "minishopify.database.windows.net"; // update me
-        $connectionOptions = array(
-            "Database" => "mini_shopify_dev", // update me
-            "Uid" => "admin_minishopify_sysc4806", // update me
-            "PWD" => "j;*86XgQa^b2+CR5" // update me
-        );
-        //Establishes the connection
-        $conn = sqlsrv_connect($serverName, $connectionOptions);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:minishopify.database.windows.net,1433; Database = mini_shopify_dev", "admin_minishopify_sysc4806", "j;*86XgQa^b2+CR5");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "admin_minishopify_sysc4806", "pwd" => "j;*86XgQa^b2+CR5", "Database" => "mini_shopify_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:minishopify.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
 
 <!DOCTYPE html>
