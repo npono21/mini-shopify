158bb768091844ebd720e9398be33d922a6df289
npono21 <nickpon123@gmail.com>
2024-11-23 12:41:56 -0500

    testing from this branch to push to app

--- src/main/java/SYSC4806Project/RegisterServlet.java
+++ src/main/java/SYSC4806Project/RegisterServlet.java
@@ -12,10 +12,10 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/registerMerchant")
 public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-        String username = request.getParameter("username");
+        String username = request.getParameter("email");
         String password = request.getParameter("password");
         try (Connection conn = DatabaseConnection.getConnection()) {
-            String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+            String sql = "INSERT INTO users (user_email, user_password) VALUES (?, ?)";
             try (PreparedStatement stmt = conn.prepareStatement(sql)) {
                 stmt.setString(1, username);
                 stmt.setString(2, password); // Use hashing for passwords!
