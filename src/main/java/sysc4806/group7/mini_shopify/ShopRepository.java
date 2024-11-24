183ef8d1e67c2220004a848585a46a3e489e3b75
Michael De Santis <michaelpeterdesantis@gmail.com>
2024-11-23 20:21:04 -0500

    add CRUDRepository methods for ShopRepository

--- src/main/java/sysc4806/group7/mini_shopify/ShopRepository.java
+++ src/main/java/sysc4806/group7/mini_shopify/ShopRepository.java
@@ -2,9 +2,13 @@ package sysc4806.group7.mini_shopify;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public interface ShopRepository extends CrudRepository<Shop, Long> {
     Optional<Shop> findById(Long id);
     Optional<Shop> findByIdAndMerchantId(Long id, Long merchantId);
+    Optional<Shop> findByName(String shopName);
+    List<Shop> findAll();
 }
