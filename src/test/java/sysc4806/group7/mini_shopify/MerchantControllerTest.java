a2aac000258a9c67a4ca100d022fcf39c7641bac
Arthur Atangana <83664112+ArthurAtangana@users.noreply.github.com>
2024-11-25 01:14:07 -0500

    Integration tests for merchant controller (#75)
    - add log settings file for tests (default is info)
    - remove findByIdAndMerchantId in MerchantController.java as shops are uniquely generated by Id, so no need to check for the correct merchant. enables better mocking
    - signin merchant test
    - show merchant home test
    - show shop home test
    - add product test
    - create shop test

--- src/test/java/sysc4806/group7/mini_shopify/MerchantControllerTest.java
+++ src/test/java/sysc4806/group7/mini_shopify/MerchantControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -18,44 +19,99 @@ public class MerchantControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void createMerchant() throws Exception {
-        String name = "bob";
-        String username = "bob@gmail.com";
-        String password = "test";
+    public void createMerchantTest() throws Exception {
         mockMvc.perform(post("/home/merchant/createMerchant")
-                        .param("name", name)
-                        .param("username", username)
-                        .param("password", password))
+                        .param("name", "bob")
+                        .param("username", "bob@gmail.com")
+                        .param("password", "test"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/home/merchant/1"));
     }
 
     @Test
-    public void addProduct() throws Exception {
-        /**
-        // post request
-        MockMultipartFile testImage = new MockMultipartFile("data", "filename.png", "text/plain", "some xml".getBytes());
-        mockMvc.perform(MockMvcRequestBuilders.multipart("/home/1/1/addProduct")
-                        .file("select_product_img", testImage.getBytes())
+    public void signingMerchantTest() throws Exception {
+        // create merchant
+        mockMvc.perform(post("/home/merchant/createMerchant")
+                .param("name", "bob")
+                .param("username", "bob@gmail.com")
+                .param("password", "test"));
+        // signing merchant
+        mockMvc.perform(post("/home/merchant/signinMerchant")
+                        .param("username", "bob@gmail.com")
+                        .param("password", "test"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/home/merchant/1"));
+    }
+
+    @Test
+    public void createShopTest() throws Exception {
+        // mock a merchant
+        mockMvc.perform(post("/home/merchant/createMerchant")
+                .param("name", "bob")
+                .param("username", "bob@gmail.com")
+                .param("password", "test"));
+        // create a shop
+        mockMvc.perform(post("/home/merchant/1/createShop")
+                        .param("shopName", "shop1")
+                        .param("shopDescription", "shop1's shop description")
+                        .param("shopTags", "COMPUTERS"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/home/merchant/1"));
+    }
+
+    @Test
+    public void showMerchantHomeTest() throws Exception {
+        // mock a merchant
+        mockMvc.perform(post("/home/merchant/createMerchant")
+                .param("name", "bob")
+                .param("username", "bob@gmail.com")
+                .param("password", "test"));
+        mockMvc.perform(get("/home/merchant/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("merchant"));
+
+    }
+
+    @Test
+    public void showShopHomeTest() throws Exception {
+        // mock a merchant
+        mockMvc.perform(post("/home/merchant/createMerchant")
+                .param("name", "bob")
+                .param("username", "bob@gmail.com")
+                .param("password", "test"));
+        // create a shop
+        mockMvc.perform(post("/home/merchant/1/createShop")
+                .param("shopName", "shop1")
+                .param("shopDescription", "shop1's shop description")
+                .param("shopTags", "COMPUTERS"));
+        // show shop home
+        mockMvc.perform(get("/home/merchant/1/shop/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("shop"))
+                .andExpect(model().attributeExists("merchant"));
+    }
+
+    @Test
+    public void addProductTest() throws Exception {
+        // mock a merchant
+        mockMvc.perform(post("/home/merchant/createMerchant")
+                .param("name", "bob")
+                .param("username", "bob@gmail.com")
+                .param("password", "test"));
+        // create a shop
+        mockMvc.perform(post("/home/merchant/1/createShop")
+                        .param("shopName", "shop1")
+                        .param("shopDescription", "shop1's shop description")
+                        .param("shopTags", "COMPUTERS"));
+        // add a product
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/home/merchant/1/shop/1/addProduct")
+                        .file("select_product_img", "image.png".getBytes())
                         .param("productName", "test")
                         .param("productDescription", "test")
                         .param("productPrice", "100")
                         .param("quantity", "1"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(model().attributeExists("product"))
-                .andExpect(redirectedUrl("/home/1/1"));
-        */
+                .andExpect(redirectedUrl("/home/merchant/1/shop/1"));
     }
 
-    @Test
-    public void showMerchantHome() {
-    }
-
-    @Test
-    public void createShop() {
-    }
-
-    @Test
-    public void showShopHome() {
-    }
 }
